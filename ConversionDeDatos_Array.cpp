5285861bba8922db8db640f99f34e3fc9ec0069b
EfrainRojas <efrainulisesrojaspina@gmail.com>
2023-01-07 01:25:19 -0600

    Funcion qu econvierte un string a entero

--- ConversionDeDatos_Array.cpp
+++ ConversionDeDatos_Array.cpp
@@ -6,10 +6,18 @@ bool esEntero(std::string cadena){
     return cadena.find_first_not_of("0123456789-") == std::string::npos;
 }
 
+//Convierte un string a int
+int stringToInt(std::string cadena){
+    return std::stoi(cadena);
+}
 
 int main () {
     std::string entrada;
     getline(std::cin, entrada);
-    bool i = esEntero(entrada);
-    std::cout << i << "\n";
+    int entero = 0;
+    if(esEntero(entrada)){
+        entero = stringToInt(entrada);
+    }
+    std::cout << entero << "\n";
+    std::cout << entero + 1 << "\n";
 }
