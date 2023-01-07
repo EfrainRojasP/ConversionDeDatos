00a4ab625bf71b590e8eff9514f18cfcff61a6f9
EfrainRojas <efrainulisesrojaspina@gmail.com>
2023-01-07 01:15:46 -0600

    Creacion de la funcion es entero

--- ConversionDeDatos_Array.cpp
+++ ConversionDeDatos_Array.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include <string>
+
+//Determina si una cadena es entero o no
+bool esEntero(std::string cadena){
+    return cadena.find_first_not_of("0123456789-") == std::string::npos;
+}
+
 
 int main () {
-    std::cout << "Hola mundo" << "\n";
+    std::string entrada;
+    getline(std::cin, entrada);
+    bool i = esEntero(entrada);
+    std::cout << i << "\n";
 }
